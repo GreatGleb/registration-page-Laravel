4c6dcf7cb2bad124167a6dd198d2ac1932d0e15e
GreatGleb <gwelbts@gmail>
2024-10-29 14:13:42 +0200

    added relationship of position in user model

--- registration-page/app/Models/User.php
+++ registration-page/app/Models/User.php
@@ -23,11 +23,10 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
+     * Get position of user
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function position(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Position::class, 'position_id');
+    }
 }
